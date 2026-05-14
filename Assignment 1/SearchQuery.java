36597c1b74d2bf6ffea41f0a20f4eefc637e961a
Cato Baba <151993405+freeanss@users.noreply.github.com>
2026-05-14 21:22:50 +0530

    Create SearchQuery.java

---
+++ Assignment 1/SearchQuery.java
@@ -0,0 +1,13 @@
+import java.rmi.*;
+import java.rmi.server.*;
+public class SearchQuery extends UnicastRemoteObject implements Search {
+
+  public String query(String search) throws RemoteException {
+        String result;
+        if (search.equals("Reflection in Java"))
+            result = "Found";
+        else
+            result = "Not Found";
+        return result;
+    }
+}
